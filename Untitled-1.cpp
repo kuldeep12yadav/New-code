a7ea095544226fbce1e258f27351eabbb1359628
kuldeep12yadav <kuldeepyadav12811@gmail.com>
2022-10-05 12:33:24 +0530

    New

---
+++ Untitled-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int a,b,c;
+    cout<<"Enter value of 'a' =\t";
+    cin >>a;
+    cout<<"Enter value of 'b' =\t";
+    cin>>b;
+
+    c = (a+b);
+
+    cout << c<< endl;
+
+}
